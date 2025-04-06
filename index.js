76b6dc292631238c1805b547e9fbc7d4598568e0
Chelsea-Saunders <168315598+Chelsea-Saunders@users.noreply.github.com>
2025-04-06 17:20:09 -0600

    about me page working

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
+const images = document.querySelectorAll(".main-button");
 
-
-//Java for add to cart button on home page
-addToCartButton = document.querySelectorAll(".add-to-cart-button");
-
-document.querySelectorAll('.add-to-cart-button').forEach(function(addToCartButton) {
-    addToCartButton.addEventListener('click', function() {
-        addToCartButton.classList.add('added');
-        setTimeout(function(){
-            addToCartButton.classList.remove('added');
-        }, 2000);
-    });
+images.forEach(function (img) {
+  img.addEventListener("click", function() {
+    const targetPage = img.getAttribute("data-link");
+    window.location.href = targetPage;
+  });
 });
-
-
-//Java for log in page on nav tab
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
