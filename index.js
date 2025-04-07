4c11522c83e879c7a48c3098a6680addfce27c88
Chelsea-Saunders <168315598+Chelsea-Saunders@users.noreply.github.com>
2025-04-06 18:25:19 -0600

    changed bio

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const images = document.querySelectorAll(".main-button");
+const toggle = document.querySelector(".dropdown-toggle");
+const menu = document.querySelector(".dropdown-menu");
 
 images.forEach(function (img) {
   img.addEventListener("click", function() {
     const targetPage = img.getAttribute("data-link");
     window.location.href = targetPage;
   });
+});
+toggle.addEventListener("click", function() {
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 });
