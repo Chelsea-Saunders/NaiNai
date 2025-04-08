c31ff474f85a1686a925976d22a604e25a5ec16d
Chelsea-Saunders <168315598+Chelsea-Saunders@users.noreply.github.com>
2025-04-07 20:17:26 -0600

    added images to lists

--- index.js
+++ index.js
@@ -16,101 +16,372 @@ const items = [
     image: "./images/imagecompressor/mr-mrs.jpg"
   }, 
   {
-    name: "Mr & Mrs Champagne Glasses",
+    name: "Tooled Leather Notebook",
     price: "$0.00",
     description: "Handmade with love and precision.",
-    image: "./images/imagecompressor/mr-mrs.jpg"
+    image: "./images/imagecompressor/notebook-outside.jpg"
   },
   {
-    name: "Mr & Mrs Champagne Glasses",
+    name: "Tooled Leather Calendars",
     price: "$0.00",
     description: "Handmade with love and precision.",
-    image: "./images/imagecompressor/mr-mrs.jpg"
+    image: "./images/imagecompressor/calendars.jpg"
   },
   {
-    name: "Mr & Mrs Champagne Glasses",
+    name: "Tooled Leather Glasses Case",
     price: "$0.00",
     description: "Handmade with love and precision.",
-    image: "./images/imagecompressor/mr-mrs.jpg"
+    image: "./images/imagecompressor/case.jpg"
   },
   {
-    name: "Mr & Mrs Champagne Glasses",
+    name: "Tooled Leather Halter",
     price: "$0.00",
     description: "Handmade with love and precision.",
-    image: "./images/imagecompressor/mr-mrs.jpg"
+    image: "./images/imagecompressor/halter.jpg"
+  },
+  {
+    name: "Tooled Pillow: Bucking Horse",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/pillow2.jpg"
+  },
+  {
+    name: "Tooled Pillow: Horse Roping",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/pillow1.jpg"
+  },
+  {
+    name: "Tooled Leather Seat Covers",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/seat-cover.jpg"
+  },
+  {
+    name: "Tooled Leather Welding Helmet",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/welding-helmet.jpg"
+  },
+  {
+    name: "Tooled Leather Picture Frame",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/picture-frame.jpg"
+  },
+  {
+    name: "Tooled Leather Mug",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/mug.jpg"
+  },
+  {
+    name: "Tooled Leather Hat: Just Livin'",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/hat2.jpg"
+  },
+  {
+    name: "Tooled Leather Mugg Hugger",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/mug-hugger.jpg"
+  },
+  {
+    name: "Tooled Leather Hat: Moonlight Cattle Co.",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/hat1.jpg"
+  },
+  {
+    name: "Tooled Leather Phone Case: Horse",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/phone-case-horse.png"
+  },
+  {
+    name: "Tooled Leather Heels",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/tasiya-heels.png"
+  },
+  {
+    name: "Tooled Leather Wallet",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/wallet.png"
+  },
+  {
+    name: "Tooled Leather Skillet Handle Guard",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/skillet-handle.png"
+  },
+  {
+    name: "Tooled Leather Berkenstocks",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/birkenstocks.png"
+  },
+  {
+    name: "Tooled Leather Leather Toed Shoes",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/leather-toed-shoes.png"
+  },
+  {
+    name: "Tooled Leather Twisted X Shoes",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/twistec-x-shoes.png"
+  },
+  {
+    name: "Tooled Leather Spur Straps",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/spurs.png"
+  },
+  {
+    name: "Tooled Leather Hair Clip",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/hair-clip.png"
+  },
+  {
+    name: "Tooled Leather Hats",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/larg-hat-order.png"
+  },
+  {
+    name: "Screen-printed Western Onsie",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/onsie.png"
+  },
+  {
+    name: "Screen-printed Western Tie",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/brown-saddle-tie-min.png"
   }
 ];
 const leather = [
   {
-    name: "Leather Birkens", 
-    price: "$0.00", 
-    description: "Leather Birkenstocks", 
+    name: "Mr & Mrs Champagne Glasses",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/mr-mrs.jpg"
+  }, 
+  {
+    name: "Tooled Leather Notebook",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/notebook-outside.jpg"
+  },
+  {
+    name: "Tooled Leather Calendars",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/calendars.jpg"
+  },
+  {
+    name: "Tooled Leather Glasses Case",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/case.jpg"
+  },
+  {
+    name: "Tooled Leather Halter",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/halter.jpg"
+  },
+  {
+    name: "Tooled Pillow: Bucking Horse",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/pillow2.jpg"
+  },
+  {
+    name: "Tooled Pillow: Horse Roping",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/pillow1.jpg"
+  },
+  {
+    name: "Tooled Leather Seat Covers",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/seat-cover.jpg"
+  },
+  {
+    name: "Tooled Leather Welding Helmet",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/welding-helmet.jpg"
+  },
+  {
+    name: "Tooled Leather Picture Frame",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/picture-frame.jpg"
+  },
+  {
+    name: "Tooled Leather Mug",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/mug.jpg"
+  },
+  {
+    name: "Tooled Leather Hat: Just Livin'",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/hat2.jpg"
+  },
+  {
+    name: "Tooled Leather Mugg Hugger",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/mug-hugger.jpg"
+  },
+  {
+    name: "Tooled Leather Hat: Moonlight Cattle Co.",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/hat1.jpg"
+  },
+  {
+    name: "Tooled Leather Phone Case: Horse",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/phone-case-horse.png"
+  },
+  {
+    name: "Tooled Leather Heels",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/tasiya-heels.png"
+  },
+  {
+    name: "Tooled Leather Wallet",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/wallet.png"
+  },
+  {
+    name: "Tooled Leather Skillet Handle Guard",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/skillet-handle.png"
+  },
+  {
+    name: "Tooled Leather Berkenstocks",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
     image: "./images/imagecompressor/birkenstocks.png"
-    },
-    {
-    name: "Leather Birkens", 
-    price: "$0.00", 
-    description: "Leather Birkenstocks", 
-    image: "./images/imagecompressor/birkenstocks.png"
-    },
-    {
-    name: "Leather Birkens", 
-    price: "$0.00", 
-    description: "Leather Birkenstocks", 
-    image: "./images/imagecompressor/birkenstocks.png"
-    },
-    {
-    name: "Leather Birkens", 
-    price: "$0.00", 
-    description: "Leather Birkenstocks", 
-    image: "./images/imagecompressor/birkenstocks.png"
-    }
+  },
+  {
+    name: "Tooled Leather Leather Toed Shoes",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/leather-toed-shoes.png"
+  },
+  {
+    name: "Tooled Leather Twisted X Shoes",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/twistec-x-shoes.png"
+  },
+  {
+    name: "Tooled Leather Spur Straps",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/spurs.png"
+  },
+  {
+    name: "Tooled Leather Hair Clip",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/hair-clip.png"
+  },
+  {
+    name: "Tooled Leather Hats",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/larg-hat-order.png"
+  }
 ];
 const wearable = [
   {
-    name: "Graphic Onsie", 
-    price: "$0.00", 
-    description: "Soft cotton onsie", 
+    name: "Screen-printed Western Onsie",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/onsie.png"
+  },
+  {
+    name: "Screen-printed Western Tie",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
     image: "./images/imagecompressor/brown-saddle-tie-min.png"
   },
   {
-    name: "Graphic Onsie", 
-    price: "$0.00", 
-    description: "Soft cotton onsie", 
-    image: "./images/imagecompressor/brown-saddle-tie-min.png"
+    name: "Tooled Leather Welding Helmet",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/welding-helmet.jpg"
   },
   {
-    name: "Graphic Onsie", 
-    price: "$0.00", 
-    description: "Soft cotton onsie", 
-    image: "./images/imagecompressor/brown-saddle-tie-min.png"
+    name: "Tooled Leather Berkenstocks",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/birkenstocks.png"
   },
   {
-    name: "Graphic Onsie", 
-    price: "$0.00", 
-    description: "Soft cotton onsie", 
-    image: "./images/imagecompressor/brown-saddle-tie-min.png"
-  }
+    name: "Tooled Leather Leather Toed Shoes",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/leather-toed-shoes.png"
+  },
+  {
+    name: "Tooled Leather Twisted X Shoes",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/twistec-x-shoes.png"
+  },
+  {
+    name: "Tooled Leather Heels",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/tasiya-heels.png"
+  },
+  {
+    name: "Tooled Leather Spur Straps",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/spurs.png"
+  },
+  {
+    name: "Tooled Leather Hair Clip",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/hair-clip.png"
+  },
+  {
+    name: "Tooled Leather Hats",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/larg-hat-order.png"
+  },
+  {
+    name: "Tooled Leather Hat: Moonlight Cattle Co.",
+    price: "$0.00",
+    description: "Handmade with love and precision.",
+    image: "./images/imagecompressor/hat1.jpg"
+  },
+
 ];
 const custom = [
-  {
-    name: "Welding Helmet", 
-    price: "$TBD", 
-    description: "Custom welding helmet",
-    image: "./images/imagecompressor/welding-helmet.jpg"
-  },
-  {
-    name: "Welding Helmet", 
-    price: "$TBD", 
-    description: "Custom welding helmet",
-    image: "./images/imagecompressor/welding-helmet.jpg"
-  },
-  {
-    name: "Welding Helmet", 
-    price: "$TBD", 
-    description: "Custom welding helmet",
-    image: "./images/imagecompressor/welding-helmet.jpg"
-  },
   {
     name: "Welding Helmet", 
     price: "$TBD", 
